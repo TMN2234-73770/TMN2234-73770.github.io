3bdb3ad057f8d91252045afb9c657e6dcf6f4ffc
TMN2234-73770 <106587660+TMN2234-73770@users.noreply.github.com>
2022-06-06 15:13:01 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,19 +1,13 @@
 <?php 
 
- $fname= $lname = $email = $phone = $password = $checked ="";
- $fnameErr= $lnameErr= $emailErr= $phoneErr= $passwordErr= $checkedErr =="";
- 
- if($_SERVER["REQUEST_METHOD"]=="POST"){
-	 if(empty($_POST["fname"])){<?php 
-
  $fname= $lname = $email = $phone = $password= $checked="";
- $fnameErr= $lnameErr= $emailErr= $phoneErr= $passwordErr= $checkedErr =="";
+ $fnameErr= $lnameErr= $emailErr= $phoneErr= $passwordErr= $checkedErr="";
  
  if($_SERVER["REQUEST_METHOD"]=="POST"){
 	 
 	 if(empty($_POST["fname"])){
 		 
-		 $fnameErr = "Required field";
+		 $fnameErr = "*required";
 		   
 	 }
 	 else
@@ -25,7 +19,7 @@
 	 }
 	 
 	 if(empty($_POST["lname"])){
-		 $lnameErr = "Required";
+		 $lnameErr = "*required";
 		 
 	 }
 	 else
@@ -37,34 +31,34 @@
 	 }
 	  
 	 if (empty($_POST["email"])) {
-		$emailErr = "Email is required";
+		$emailErr = "*required";
 	} 
 	else 
 	{
-		
+	
 		$email = test_input($_POST["email"]);
-			if (!preg_match("/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9_\.\-])+\.)+([a-zA-Z0-9]{2,4})+$/", $email)) {
+		if (!preg_match("/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9_\.\-])+\.)+([a-zA-Z0-9]{2,4}) $/", $email)) {
     }
   }
 	 
 	if(empty($_POST["phone"])){
-		 $phoneErr = "Required";
+		 $phoneErr = "*required";
 		 
 	 }
 	 else
 	 { 
 		$phone = test_input($_POST["phone"]); 
-			if(!preg_match("/^\+?([0-9]{2})\)?[-. ]?([0-9]{3,4})[-, ]?([0-9]{4})$/", $phone)){
+			if(!preg_match("/^\+?([0-9]{2})\)?[-. ]?([0-9]{3,4})[-. ]?([0-9]{4,5})$/", $phone)){
 				$phoneErr = "Please enter a valid mobile number For example: +xx-xxxxxxxxx";
 			}
 	 }
 	
 	    if(!empty($_POST["password"]))
 		{
-			$passwordErr = "Required";
+			$passwordErr = "*required";
 		}
 		else{
-            if (!preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]). {6,}$/", $password))
+            if (!preg_match("/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/", $password))
             {
                 $passwordErr = "Password should be 6 digits length contain ONE  uppercase ONE lowercase ONE special character  ";
             }
@@ -76,7 +70,7 @@
 			$checkedErr = "Please agree to the Terms and Conditions";
 			}
  }
- function test_input(&data)
+ function test_input($data)
  {
 	 
 	 $data = trim($data);
@@ -93,84 +87,4 @@
 	 }
 	
  include('form.html')
-?> 
-		 $fnameErr = "Required";
-		 
-	 }
-	 else
-	 { 
-		$fname = test_input ($_POST["fname"]); 
-			if(!preg_match("/^(\b[A-Z][a-z]*\s*)+$/", $fname)){
-				$fnameErr = "first character uppercase and follow by lowercase";
-			}
-	 }
-	 
-	 if(empty($_POST["lname"])){
-		 $lnameErr = "Required";
-		 
-	 }
-	 else
-	 { 
-		$lname = test_input ($_POST["lname"]); 
-			if(!preg_match("/^(\b[A-Z][a-z]*\s*)+$/", $lname)){
-				$fnameErr = "first character uppercase and follow by lowercase";
-			}
-	 }
-	  
-	 if (empty($_POST["email"])) {
-		$emailErr = "Email is required";
-	} 
-	else 
-	{
-		
-		$email = test_input($_POST["email"]);
-			if (!preg_match("/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9_\.\-])+\.)+([a-zA-Z0-9]{2,4})+$/", $email)) {
-    }
-  }
-	 
-	if(empty($_POST["phone"])){
-		 $phoneErr = "Required";
-		 
-	 }
-	 else
-	 { 
-		$phone = test_input ($_POST["phone"]); 
-			if(!preg_match("/^\+?([0-9]{2})\)?[-. ]?([0-9]{3,4})[-, ]?([0-9]{4})$/", $phone)){
-				$phoneErr = "Please enter a valid mobile number For example: +xx-xxxxxxxxx";
-			}
-	 }
-	
-	    if(!empty($_POST["password"]))
-		{
-			$passwordErr = "Required";
-		}
-		
-            if (!preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]). {6,}$/", $password))
-            {
-                $passwordErr = "Password should be 6 digits length contain ONE  uppercase ONE lowercase ONE special character  ";
-            }
-     
-        }
-		
-		if(!isset($_POST["accept"]))
-		{
-			$checkedErr = "Please agree to the Terms and Conditions";
-			}
- }
- function test_input(&data){
-	 
-	 $data = trim($data);
-	 $data = stripslashes($data);
-	 $data = htmlspecialchars($data);
-	 return $data;
- }
-	 if(isset($_POST['Register']))
-	 { 
-		if($fnameErr ==""&& $lnameErr ==""&& $emailErr ==""&& $phoneErr ==""&& $passwordErr ==""&& $checkedErr ==""){
-			echo "Register successful";
-		}
-		 
-	 }
-	
- include("index.html")
 ?> 
